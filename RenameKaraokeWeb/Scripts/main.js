1c7d245945ccd7b6609a9970677806e496ac4a13
James Champion <jameschampion1123@gmail.com>
2019-02-18 07:24:46 -0500

    Second Commit

--- RenameKaraokeWeb/Scripts/main.js
+++ RenameKaraokeWeb/Scripts/main.js
@@ -9,7 +9,7 @@ document.getElementById('submit-btn').addEventListener('click', function () {
 
 document.getElementById('textFileInput').addEventListener('change', function () {
     var fileName = $(this).val();
-    document.getElementById('title-Text').textContent = filename;
+    console.log(fileName);
 }
     );
 
