ffd01ae5c6a77ee21e8b9788bd97ec66483613e2
James Champion <jameschampion1123@gmail.com>
2019-02-20 15:11:15 -0500

    Added a Reference TO CSV and Began Parsing

--- RenameKaraokeWeb/Controllers/TitleController.cs
+++ RenameKaraokeWeb/Controllers/TitleController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CsvHelper;
+using RenameKaraokeWeb.Models;
 
 namespace RenameKaraokeWeb.Controllers
 {
@@ -16,11 +19,30 @@ namespace RenameKaraokeWeb.Controllers
         [HttpPost]
         public ActionResult Table(HttpPostedFileBase songList)
         {
-            // 1) parse file
-            // 2) build model
-            // 3) pass model to view -    return View(model);
+            using (var reader = new StreamReader(songList.InputStream))
+                using (var csv = new CsvReader(reader))
+            {
+                csv.Configuration.Delimiter = " - ";
+                csv.Configuration.HasHeaderRecord = false;
+                csv.Configuration.MissingFieldFound = null;
+                var records = csv.GetRecords<SongRecord>();
+                foreach(SongRecord record in records)
+                {
 
-            return View();
+                    Console.WriteLine(record.ID + " " + record.Artist + " " + record.Title);
+                }
+
+
+            }
+
+
+
+
+                // 1) parse file
+                // 2) build model
+                // 3) pass model to view -    return View(model);
+
+                return View();
         }
     }
 }
--- RenameKaraokeWeb/Models/SongRecord.cs
+++ RenameKaraokeWeb/Models/SongRecord.cs
@@ -3,6 +3,8 @@ namespace RenameKaraokeWeb.Models
 {
     public class SongRecord
     {
-        public string Name { get; set; }
+        public string ID { get; set; }
+        public string Artist { get; set; }
+        public string Title { get; set; }
     }
 }
